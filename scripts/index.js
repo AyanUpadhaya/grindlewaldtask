4fc291b79e12347cbda5e13e95b1e935df385a66
Ayan Upadhaya <ayanu881@gmail.com>
2024-07-07 03:04:56 +0600

    carousel added

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,23 @@
-console.log("Working");
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
